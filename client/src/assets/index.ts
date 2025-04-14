e4f39b34c0481c56833305905132a4982550c86d
falbor <falbor89@gmail.com>
2025-04-14 22:38:13 +0200

    added foto

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -5,3 +5,12 @@ export { default as stylebg } from './images/profile1.svg';
 
 // Images
 export { default as ProfilePic1 } from './images/profile1.svg';
+export { default as ProfilePic2 } from './images/icon_0_0.png';
+export { default as ProfilePic3 } from './images/icon_0_1.png';
+export { default as ProfilePic4 } from './images/icon_0_2.png';
+export { default as ProfilePic5 } from './images/icon_1_0.png';
+export { default as ProfilePic6 } from './images/icon_1_1.png';
+export { default as ProfilePic7 } from './images/icon_1_2.png';
+export { default as ProfilePic8 } from './images/icon_2_0.png';
+export { default as ProfilePic9 } from './images/icon_2_1.png';
+export { default as ProfilePic10 } from './images/icon_2_2.png';
